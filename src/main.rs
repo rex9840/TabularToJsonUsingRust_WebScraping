8e24e51daf3d5d1b87928c538fd85a64bfc8925f
Rupesh Nepal <rupeshrnepal@gmail.com>
2023-09-20 14:45:14 +0545

    url for the site changed

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ mod Request;
 
 fn main()
 { 
-    let url = r#"https://www.worldometers.info/coronavirus/#countries"#;
+    let url = r#"https://merolagani.com/LatestMarket.aspx"#;
     let html =Request::get_html(url).unwrap(); 
 
     let document  = Html::parse_document(&html);
-    let selector = Selector::parse(r#"div[class="maincounter-number"]"#).unwrap();
+    let selector = Selector::parse(r#"table"#).unwrap();
 
 
     let input = document.select(&selector).next().unwrap(); 
 
-    println!("{0:?}",input.text().collect::<Vec<_>>());
+    println!("{0:?}",input.html());
 
 
 }
