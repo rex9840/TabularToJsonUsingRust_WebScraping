f46317624a5483ef0da7ddbf29d4ae8fdee00afc
Rupesh Nepal <rupeshrnepal@gmail.com>
2023-09-20 14:27:09 +0545

    added some adstraction bt creating some modules

---
+++ src/Request/mod.rs
@@ -0,0 +1,46 @@
+use crate::{FileOp ,FileOp::FileInfo};
+use reqwest;
+
+
+pub fn get_html(url:&str)->Result<String,reqwest::Error>
+{ 
+    
+    if reqwest::blocking::get(url).is_ok()
+    {
+        let response = reqwest::blocking::get(url)?;
+        println!("STATUS:{0:?}",response.status());
+
+    if  response.status().is_success()
+    {
+        let html = response.text()?; 
+        let raw_data = doc_raw_data(doc_create(&html,FileOp::FileInfo::default()).expect("CreationERROR"));
+        return Ok(raw_data.unwrap())
+    }
+    else
+    {
+        let raw_data = doc_raw_data(FileOp::FileInfo::default());
+        return Ok(raw_data.unwrap()); 
+    }
+    }
+    else
+    {
+        let raw_data = doc_raw_data(FileOp::FileInfo::default());
+        return Ok(raw_data.unwrap());     
+    }
+    
+}
+
+
+
+pub fn doc_create(raw_data:&str,file_info:FileInfo)-> std::io::Result<FileOp::FileInfo>
+{ 
+    file_info.doc(raw_data).expect("Error creating file"); 
+    return Ok(file_info);
+}
+
+fn doc_raw_data(file:FileOp::FileInfo)->std::io::Result<String>
+{
+    println!("{0:#?}",file);
+    let data:String =file.return_raw_data()?;
+    return Ok(data.to_owned());
+} 
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,11 @@
-use reqwest::{self, Response, dns::Resolving};
 use scraper::{Html,Selector}; 
-use std::{fs::File, default};
-
-use crate::FileOp::FileInfo; 
 mod FileOp;
-
-fn get_html(url:&str)->Result<String,reqwest::Error>
-{ 
-    
-    if reqwest::blocking::get(url).is_ok()
-    {
-        let response = reqwest::blocking::get(url)?;
-        println!("STATUS:{0:?}",response.status());
-
-    if  response.status().is_success()
-    {
-        let html = response.text()?; 
-        let raw_data = doc_raw_data(doc_create(&html,FileOp::FileInfo::default()).expect("CreationERROR"));
-        return Ok(raw_data.unwrap())
-    }
-    else
-    {
-        let raw_data = doc_raw_data(FileOp::FileInfo::default());
-        return Ok(raw_data.unwrap()); 
-    }
-    }
-    
-    else
-    {
-        let raw_data = doc_raw_data(FileOp::FileInfo::default());
-        return Ok(raw_data.unwrap());     
-    }
-    
-}
-
-
-
-fn doc_create(raw_data:&str,file_info:FileInfo)-> std::io::Result<FileOp::FileInfo>
-{ 
-    println!("{0:#?}",file_info);
-    file_info.doc(raw_data).expect("Error creating file"); 
-    return Ok(file_info);
-}
-
-fn doc_raw_data(file:FileOp::FileInfo)->std::io::Result<String>
-{
-    let data:String =file.return_raw_data()?;
-    return Ok(data.to_owned());
-} 
-
+mod Request;
 
 fn main()
 { 
     let url = r#"https://www.worldometers.info/coronavirus/#countries"#;
-    let html =get_html(url).unwrap(); 
+    let html =Request::get_html(url).unwrap(); 
 
     let document  = Html::parse_document(&html);
     let selector = Selector::parse(r#"div[class="maincounter-number"]"#).unwrap();
