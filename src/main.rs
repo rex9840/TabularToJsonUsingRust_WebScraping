7815f9db8e4590de2cac26e3dc29059e49b26562
Rupesh Nepal <rupeshrnepal@gmail.com>
2023-09-21 14:38:42 +0545

    correct syntax and logics

--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,10 @@ fn main()
 //html doc  parser
     let document  = Html::parse_document(&html);
     let table_selector = Selector::parse(r#"table"#).unwrap();
-    let input = document.select(&table_selector).next().unwrap(); 
+    let input = document.select(&table_selector).next().unwrap(); //gets  thead
 //table parser 
-    let table_feild_select  = Selector::parse(r#"tr"#).unwrap();
-    let table_feilds = input.select(&table_feild_select).next().unwrap().text().into_iter();
+    let table_feild_select  = Selector::parse(r#"thead>tr"#).unwrap();
+    let table_feilds = input.select(&table_feild_select);
 //json conversion    
     let mut json_obj = json::object::Object::new();
     let mut feild_len = 0 ;
@@ -35,31 +35,40 @@ fn main()
     let mut keys = Vec::new();
     for feild in table_feilds
     {
-        keys.push(feild);
-        json_obj.insert(feild,json::JsonValue::Null);        
+        for data in feild.text().into_iter()
+        {
+        keys.push(data);
+        json_obj.insert(data,json::JsonValue::Null);        
         feild_len+=1;   
+    }
     }
     let  mut json_list = json::array![];
 //table row parser
-    let table_body_select = Selector::parse(r#"tbody"#).unwrap();
-    let table_body = input.select(&table_body_select).next().unwrap().text().into_iter();
+    let table_body_select = Selector::parse(r#"tbody>tr"#).unwrap();
+    let table_body = input.select(&table_body_select);
 
 
     println!("\n{0:#?}\n",json_list.dump());
     println!("\n{0:#?}\n",keys); 
     for rows in table_body
     {
-        json_obj.insert(keys[counter],json::JsonValue::String(rows.to_string()));
+        for data in rows.text().into_iter()
+        {
+            println!("{0:?}:{1:?}->{2:?}",counter,keys[counter],data);
+        
+        json_obj.insert(keys[counter],json::JsonValue::String(data.to_string()));
         counter+=1;
+        
         if counter == feild_len
         {
             let _ = json_list.push(json_obj.to_owned()); 
             counter = 0;
         }    
+        
+        }
+    
     }
-
     println!("{0:#?}",json_list.dump());
     let _ = json_dump_file(json_list); 
-
 
 }
